b57d5229bf3d3fefd0cb9e1c25d4c615b2345dfa
phongngo <phongnq2712@gmail.com>
2021-09-16 21:09:59 +0700

    reup App.js

---
+++ App.js
@@ -0,0 +1,30 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const App = ()=> {
+  return(
+    <View>
+      <SafeAreaView>
+        <Text>Hi Phong!!!</Text>
+      </SafeAreaView>
+    </View>
+  );
+}
+
+export default App;
