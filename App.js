7bf542f1efc64627f0305b1d910a0032018fb218
phongngo <phongnq2712@gmail.com>
2021-09-16 23:10:00 +0700

    update layout

--- App.js
+++ App.js
@@ -13,18 +13,98 @@ import {
   StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
+  TextInput,
+  ImageBackground,
+  TouchableOpacity,
   View,
 } from 'react-native';
 
 const App = ()=> {
-  return(
-    <View>
-      <SafeAreaView>
-        <Text>Hi Phong!!!</Text>
-      </SafeAreaView>
-    </View>
+  return (
+    <ImageBackground
+      source={require("./assets/background.jpg")}
+      style={{ width: "100%", height: "100%" }}
+    >
+      <View style={styles.container}>
+        <Text
+          style={styles.lblTitle}
+        >
+          BMI Calculator
+        </Text>
+        <View style={styles.subView}>
+          <Text style = {styles.lblSubTitle}>My Height</Text>
+          <TextInput
+            placeholder="cm"
+            keyboardType="numeric"
+            style={styles.input}
+            
+          />
+        </View>
+        <View style={styles.subView}>
+          <Text style = {styles.lblSubTitle}>My Weight</Text>
+          <TextInput
+            placeholder="kg"
+            keyboardType="numeric"
+            style={styles.input}
+            
+          />
+        </View>
+        <View>
+          <TouchableOpacity
+            style = {styles.submitButton}
+            >
+            <Text style = {styles.submitButtonText}> Calculate </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </ImageBackground>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  subView: {
+    flexDirection: 'row'
+  },
+  lblTitle: {
+    color: "#830587",
+    fontWeight: 'bold',   
+    justifyContent: "center",
+    alignSelf: "center",
+    marginTop: 30,
+    fontSize: 36
+  },
+  lblSubTitle: {
+    color: '#ff6666',
+    fontWeight: 'normal',   
+    justifyContent: "center",
+    alignSelf: "center",
+    marginTop: 10,
+    marginLeft: 20,
+    fontSize: 20
+  },
+  input: {
+    height: 80,
+    textAlign: "center",
+    width: "50%",
+    fontSize: 24,
+    marginTop: 10,
+    color: "#FFCB1F"
+  },
+  submitButton: {
+    backgroundColor: '#036980',
+    padding: 10,
+    margin: 25,
+    height: 40,
+ },
+ submitButtonText:{
+    textAlign: "center",
+    color: 'white',
+    fontSize: 18,
+ },
+  
+});
 
 export default App;
